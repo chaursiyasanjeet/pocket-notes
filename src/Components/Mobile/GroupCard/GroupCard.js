3b676e793e3f90e1d50bdda260153d3000baf316
sanjeet kumar <skc0845@gmail.com>
2023-09-29 15:47:24 +0530

    mobile bug fix

--- src/Components/Mobile/GroupCard/GroupCard.js
+++ src/Components/Mobile/GroupCard/GroupCard.js
@@ -1,41 +1,52 @@
 import "./GroupCard.css";
-import { useNavigate, } from "react-router-dom";
-import { useContext, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { useContext, useRef, useEffect } from "react";
 import NoteContext from "../../Context/NoteContext";
 
-
 function GroupCard({ groupName, color }) {
-    const selectGroupContext = useContext(NoteContext)
-    const selectGroup = useRef(null)
-    const redirect = useNavigate()
+  const selectGroupContext = useContext(NoteContext);
+  const selectGroup = useRef(null);
+  const redirect = useNavigate();
 
-    //to get name logo
-    const logo = groupName ? (/\s/.test(groupName) ? groupName
-        .split(" ")
-        .map((word) => word.charAt(0))
-        .join("")
-        .toUpperCase() : groupName.slice(0, 2).toUpperCase()) : []
+  //to get name logo
+  const logo = groupName
+    ? /\s/.test(groupName)
+      ? groupName
+          .split(" ")
+          .map((word) => word.charAt(0))
+          .join("")
+          .toUpperCase()
+      : groupName.slice(0, 2).toUpperCase()
+    : [];
 
+  const handleSelect = (e) => {
+    const groupSelected = selectGroup.current.innerText;
+    selectGroupContext.setSelected(groupSelected);
+    localStorage.setItem("selected", JSON.stringify(groupSelected));
+    redirect("/notescontent");
+  };
 
-    const handleSelect = (e) => {
-        redirect('/notescontent')
-        const groupSelected = selectGroup.current.innerText
-        selectGroupContext.setSelected(groupSelected)
-        localStorage.setItem('selected', JSON.stringify(groupSelected))
-    };
+  useEffect(() => {
+    localStorage.setItem("selected", JSON.stringify());
+  }, []);
 
-    return (
-        <div
-            onClick={handleSelect}
-            style={{ backgroundColor: "" }}
-            className="card-container-mobile"
-        >
-            <div className="group-logo-mobile" style={{ backgroundColor: color ? color : "rgba(204, 204, 204, 1)" }}>
-                {logo}
-            </div>
-            <h3 ref={selectGroup} className="card-group-name-mobile">{groupName}</h3>
-        </div>
-    );
+  return (
+    <div
+      onClick={handleSelect}
+      style={{ backgroundColor: "" }}
+      className="card-container-mobile"
+    >
+      <div
+        className="group-logo-mobile"
+        style={{ backgroundColor: color ? color : "rgba(204, 204, 204, 1)" }}
+      >
+        {logo}
+      </div>
+      <h3 ref={selectGroup} className="card-group-name-mobile">
+        {groupName}
+      </h3>
+    </div>
+  );
 }
 
 export default GroupCard;
--- src/Components/Mobile/Sidebar/Sidebar.js
+++ src/Components/Mobile/Sidebar/Sidebar.js
@@ -5,53 +5,49 @@ import GroupCreatorPopup from "../GroupCreatorPopup/GroupCreatorPopup";
 import NoteContext from "../../Context/NoteContext";
 
 function Sidebar() {
-    const [sendData, setSendData] = useState()
-    const dataContext = useContext(NoteContext)
-    const displayGroupCreator = () => {
-        const show = document.getElementsByClassName("group-container-mobile");
-        show[0].style.display = "block";
-    };
+  const dataContext = useContext(NoteContext);
+  const displayGroupCreator = () => {
+    const show = document.getElementsByClassName("group-container-mobile");
+    show[0].style.display = "block";
+  };
 
-    useEffect(() => {
-        const data = localStorage.getItem("notesData");
-        if (data) {
-            dataContext.setNotesData(JSON.parse(data));
-        } else {
-            dataContext.setNotesData([]);
-        }
-    }, []);
+  useEffect(() => {
+    const data = localStorage.getItem("notesData");
+    if (data) {
+      dataContext.setNotesData(JSON.parse(data));
+    } else {
+      dataContext.setNotesData([]);
+    }
+  }, []);
 
-    const select = JSON.parse(localStorage.getItem('selected'))
-    const data = dataContext.notesData ? Object.entries(dataContext.notesData) : [];
-    useEffect(() => {
-        setSendData(data.map((item) => {
-            return (
-                <GroupCard
-                    key={item[0]}
-                    groupName={item[1].groupName}
-                    color={item[1].color}
-                    selected={item[1].isSelected}
-                />
-            );
-        }));
-    }, [])
-
-    //to convert Object into array
-
-
-
-
+  const data = dataContext.notesData
+    ? Object.entries(dataContext.notesData)
+    : [];
+  const done = data.map((item) => {
     return (
-        <>
-            <div className="mobile_sidebar">
-                <h1>Pocket Notes</h1>
-                <button onClick={displayGroupCreator} className="note-create-button-mobile">
-                    <b>+</b> Create Notes group
-                </button>
-            </div>
-            <div className="all_Groups_Sidebar_mobile">{sendData}</div>
-            <GroupCreatorPopup />
-        </>
+      <GroupCard
+        key={item[0]}
+        groupName={item[1].groupName}
+        color={item[1].color}
+        selected={item[1].isSelected}
+      />
     );
+  });
+
+  return (
+    <>
+      <div className="mobile_sidebar">
+        <h1>Pocket Notes</h1>
+        <button
+          onClick={displayGroupCreator}
+          className="note-create-button-mobile"
+        >
+          <b>+</b> Create Notes group
+        </button>
+      </div>
+      <div className="all_Groups_Sidebar_mobile">{done}</div>
+      <GroupCreatorPopup />
+    </>
+  );
 }
 export default Sidebar;
